22883d7e3753ed58c03f2820aad18648bd4deedc
Dhanush <100850081+Dhanush029@users.noreply.github.com>
2026-08-24 16:47:01 +0800

    format code

--- LED_Blinking_Builtin/LED_Blinking_Builtin.ino
+++ LED_Blinking_Builtin/LED_Blinking_Builtin.ino
@@ -1,11 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(13, OUTPUT);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(13, LOW);
   delay(700);
   digitalWrite(13, HIGH);
