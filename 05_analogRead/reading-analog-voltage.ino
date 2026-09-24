45abb0c97d1e4cdfe83583896092f52227402c63
Dhanush <100850081+Dhanush029@users.noreply.github.com>
2026-09-24 17:27:15 +0800

    Add files via upload

---
+++ 05_analogRead/reading-analog-voltage.ino
@@ -0,0 +1,18 @@
+int readPin = A3;
+float v2 = 0;
+int delaytime = 500;
+int readVal;
+
+void setup() {
+  pinMode(readPin, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  readVal = analogRead(readPin);
+  v2 = (5./1023.)*readVal;
+  Serial.println(v2);
+  delay(delaytime);
+
+}
